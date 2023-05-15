cd49e7b60e2da9d40751386bfd0e9b7e85a6e5e6
gsl <gslarsen2718@gmail.com>
2023-05-15 17:59:00 -0400

    first django app

--- api/coronavstech/companies/models.py
+++ api/coronavstech/companies/models.py
@@ -10,7 +10,12 @@ class Company(models.Model):
         HIRING = "Hiring"
 
     name = models.CharField(max_length=30, unique=True)
-    status = models.CharField(max_length=30, choices=CompanyStatus.choices, default=CompanyStatus.HIRING)
+    status = models.CharField(
+        max_length=30, choices=CompanyStatus.choices, default=CompanyStatus.HIRING
+    )
     last_update = models.DateTimeField(default=now, editable=True)
     application_link = URLField(blank=True)
     notes = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
