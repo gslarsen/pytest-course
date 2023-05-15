7757e090ddc7f464df0e433b656c91d224ec3014
gsl <gslarsen2718@gmail.com>
2023-05-15 17:32:59 -0400

    add models.py and amend settings.py

--- api/coronavstech/companies/models.py
+++ api/coronavstech/companies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models import URLField
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Company(models.Model):
+    class CompanyStatus(models.TextChoices):
+        LAYOFFS = "Layoffs"
+        HIRING_FREEZE = "Hiring Freeze"
+        HIRING = "Hiring"
+
+    name = models.CharField(max_length=30, unique=True)
+    status = models.CharField(max_length=30, choices=CompanyStatus.choices, default=CompanyStatus.HIRING)
+    last_update = models.DateTimeField(default=now, editable=True)
+    application_link = URLField(blank=True)
+    notes = models.CharField(max_length=100, blank=True)
