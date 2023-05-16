9922212548efa32ddb33e7de073d6d3b4763585a
Greg Larsen <gslarsen2718@gmail.com>
2023-05-16 17:49:50 -0400

    add initial pytest

---
+++ api/coronavstech/companies/tests/test_api.py
@@ -0,0 +1,31 @@
+import json
+from unittest import TestCase
+
+import pytest
+from django.test import Client
+from django.urls import reverse
+
+@pytest.mark.django_db
+class TestGetCompanies(TestCase):
+    def test_zero_companies_should_return_empty_list(self) -> None:
+        client = Client()
+        companies_url = reverse("companies-list")
+        response = client.get(companies_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content), [])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
