45e34fef77ee8395caa852fcff5e8adcdf1865c8
Greg Larsen <gslarsen2718@gmail.com>
2023-05-16 18:13:58 -0400

    add tests for adding one company to test db

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -5,6 +5,9 @@ import pytest
 from django.test import Client
 from django.urls import reverse
 
+from companies.models import Company
+
+
 @pytest.mark.django_db
 class TestGetCompanies(TestCase):
     def test_zero_companies_should_return_empty_list(self) -> None:
@@ -13,6 +16,31 @@ class TestGetCompanies(TestCase):
         response = client.get(companies_url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), [])
+
+    def test_one_company_exists_should_succeed(self) -> None:
+        client = Client()
+        test_company = Company.objects.create(name="Amazon")
+        companies_url = reverse("companies-list")
+        response = client.get(companies_url)
+        response_content = json.loads(response.content)[0]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_content.get("name"), test_company.name)
+        self.assertEqual(response_content.get("status"), "Hiring")
+        self.assertEqual(response_content.get("application_link"), "")
+        self.assertEqual(response_content.get("notes"), "")
+
+        test_company.delete()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
