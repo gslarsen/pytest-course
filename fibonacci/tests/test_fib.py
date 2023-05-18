9e5fcd03b4fc59880f3e8a6267cedb06e57aa574
Greg Larsen <gslarsen2718@gmail.com>
2023-05-18 09:14:49 -0400

    amend bitbucket pipeline

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -1,9 +1,15 @@
+from typing import Callable
+
 import pytest
 
+from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
 from fibonacci.naive import fibonacci_naive
 
 
+@pytest.mark.parametrize(
+    "fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached]
+)
 @pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
-def test_naive(n: int, expected: int) -> None:
-    res = fibonacci_naive(n=n)
+def test_fibonacci(fib_func: Callable[[int], int], n: int, expected: int) -> None:
+    res = fib_func(n)
     assert res == expected
