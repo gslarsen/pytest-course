0867cf2c2bcd104955ac55e557be577c7ae15027
Greg Larsen <gslarsen2718@gmail.com>
2023-05-18 09:21:14 -0400

    add fibonacci cached dynamic programming with order for tests

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -5,11 +5,12 @@ import pytest
 from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
 from fibonacci.naive import fibonacci_naive
 
+
 # Cached solutions
+@pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
 @pytest.mark.parametrize(
     "fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached]
 )
-@pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
 def test_fibonacci(fib_func: Callable[[int], int], n: int, expected: int) -> None:
     res = fib_func(n)
     assert res == expected
